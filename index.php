0d0ad5fc2c0503e7b76ccaa3957d73f798addcb0
Levi <levi@teklife.info>
2015-11-14 00:08:44 -0500

    Moved CSS to a css file.

--- index.php
+++ index.php
@@ -20,78 +20,7 @@
   <meta name="description" content="<?php echo $description; ?>" />
   <meta http-equiv="last-modified" content="<?php echo $modified; ?>">
   <link rel='canonical' href='https://wappr.co/' />
-  <style>
-    html {
-        height:100%;
-    }
-    body {
-        background-color:#2ecc71;
-        height:100%;
-        position:relative;
-        margin:0;
-        padding:0;
-        font-size:62.5%;
-    }
-    #header {
-      background-color: #2c3e50;
-      border-bottom:5px solid #27ae60;
-    }
-    #content {
-      background-color:#2c3e50;
-      border-bottom:5px solid #27ae60;
-      min-height:50%;
-    }
-    #footer {
-      background-color: #2c3e50;
-      border-bottom:5px solid #27ae60;
-      bottom:;
-    }
-    #header, #content, #footer {
-      width:80%;
-      margin: auto;
-      padding:2.5%;
-    }
-    h1 {
-        font-size:4.8em;
-        margin:0;
-        padding:0;
-    }
-    h2 { font-size:3.8em;}
-    p, li { font-size:2.5em; }
-    p {margin-top:5px;}
-    h1, h2, p, li, small {
-        color:#ecf0f1;
-        font-family: "Arial Narrow",Arial,sans-serif
-    }
-    .lead { font-size: 3.2em;}
-    ul {
-      margin-left:40px;
-    }
-    #footer p {
-      font-size:small;
-    }
-
-    a {color:#2ecc71;}
-
-    ::selection {
-	  background: #2ecc71;
-	}
-	::-moz-selection {
-	  background: #2ecc71;
-	}
-
-    @media screen and (max-width: 799px) {
-      #header, #content, #footer {
-        min-width:90%;
-      }
-      h1 { font-size:3.8em; }
-      h2 { font-size:2.8em; }
-      p, li { font-size: 1.8em;}
-      ul {
-        margin-left:0px;
-      }
-    }
-  </style>
+  <link href="/styles.css" rel="stylesheet" type="text/css">
   <!--[if lt IE 9]>
   <script src="https://html5shiv.googlecode.com/svn/trunk/html5.js"></script>
   <![endif]-->
